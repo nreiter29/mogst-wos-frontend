f1155a65df9919214215810d4c096f6a20136775
Noah Reiter <n.reiter@alpin11.com>
2023-01-02 17:05:49 +0100

    end of the workday

---
+++ compounds/SearchBar.tsx
@@ -0,0 +1,58 @@
+import { Search2Icon } from '@chakra-ui/icons'
+import type { InputProps } from '@chakra-ui/react'
+import { IconButton, Input, InputGroup, InputRightElement } from '@chakra-ui/react'
+import React from 'react'
+
+interface IColors {
+    colorBackground?: string
+    colorText?: string
+    colorAccent?: string
+    colorIcon?: string
+}
+
+export const SearchBar: React.FC<IColors> = ({
+    colorBackground = 'secondaryBackground.500',
+    colorAccent = 'accent.650',
+    colorText = 'secondaryText.900',
+    colorIcon = 'secondaryText.500',
+    ...inputProps
+}) => {
+    return (
+        <InputGroup>
+            <Input
+                w="100%"
+                backgroundColor={colorBackground}
+                fontSize="md"
+                fontWeight="medium"
+                color={colorText}
+                placeholder="Artikel suchen"
+                type="search"
+                borderRadius="20"
+                padding="5"
+                _placeholder={{
+                    opacity: 0.6,
+                    color: 'black',
+                }}
+                transitionProperty="outline-color, box-shadow"
+                transitionTimingFunction="ease-in-out, ease-in-out"
+                transitionDuration="0.15s, 0.15s"
+                _focus={{
+                    outlineWidth: '1.5px',
+                    outlineStyle: 'solid',
+                    outlineColor: colorAccent,
+                }}
+                {...inputProps}
+            />
+            <InputRightElement>
+                <IconButton
+                    aria-label="Search database"
+                    variant="link"
+                    icon={<Search2Icon />}
+                    fontSize="18"
+                    pr="4"
+                    color={colorIcon}
+                />
+            </InputRightElement>
+        </InputGroup>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
-import { Box, Button, Grid, Heading, HStack, Image, SimpleGrid, Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Grid, Heading, HStack, Image, SimpleGrid, Text, useDisclosure } from "@chakra-ui/react";
 import Link from "next/link";
 import { useCallback, useEffect, useMemo, useState } from "react";
+import { SearchBar } from "../compounds/SearchBar";
 import toBusinessNumber from "../helper/toBusinessNumber";
+import { useDebounce } from "../helper/useDebounce";
 import fetchData from "../operations/fetcher"
+import useSearchQuery from "../operations/search";
 
 const Home = () => {
   const [products, setProducts] = useState<{
@@ -30,9 +33,46 @@ const Home = () => {
     fetchProducts()
   }, [])
 
+  type SearchQuery = {
+    __typename?: "Query" | undefined;
+    search: {
+      __typename?: 'SearchResponse';
+      items: Array<{
+        __typename?: 'SearchResult';
+        id: string;
+        sku: string;
+        slug: string;
+        productId: string;
+        productName: string;
+        productVariantId: string;
+        productVariantName: string;
+        description: string;
+      }>;
+    };
+  }
+
+  const searchBar = useDisclosure()
+  const [searchInput, setSearchInput] = useState('')
+  const debouncedSearchInput = useDebounce(searchInput, 500)
+  const { data, isLoading, fetchStatus, refetch } = useSearchQuery({ input: debouncedSearchInput }, { enabled: false })
+  const [searchResults, setSearchResults] = useState<SearchQuery | undefined>()
+
+  useEffect(() => {
+    if (debouncedSearchInput.length > 2) {
+      refetch()
+    }
+    setSearchResults(data)
+    return () => {
+      setSearchResults(undefined)
+    }
+  }, [data, debouncedSearchInput.length, refetch])
+
   return (
     <Box>
-      <Box mx="auto" maxW={{ base: "2xl", lg: "7xl" }} py={{ base: "16", sm: "24" }} px={{ base: "4", sm: "6", lg: "8" }}>
+      <Flex w={{ base: "85%", lg: "40%" }} mx="auto" mt="8" px={{ base: "4", lg: "0" }}>
+        <SearchBar></SearchBar>
+      </Flex>
+      <Box mx="auto" maxW={{ base: "2xl", lg: "7xl" }} py={{ base: "6", sm: "0" }} px={{ base: "4", sm: "6", lg: "8" }}>
         <Heading as="h2" p="2">Products</Heading>
         <SimpleGrid columns={{ base: 1, sm: 2, lg: 3, xl: 4 }}>
           {products?.data.products.items.map((product) => {
@@ -50,8 +90,8 @@ const Home = () => {
                     />
                   </Box>
                   <HStack justify="space-between" align="center" py="2" fontSize="sm">
-                    <Heading as="h3" fontSize="sm" textColor="gray.700">{product.variants[0].name}</Heading>
-                    <Text textColor="gray.900">{toBusinessNumber(product.variants[0].priceWithTax)} €</Text>
+                    <Heading as="h3" fontSize="sm">{product.variants[0].name}</Heading>
+                    <Text>{toBusinessNumber(product.variants[0].priceWithTax)} €</Text>
                   </HStack>
                 </Link>
               </Box>
