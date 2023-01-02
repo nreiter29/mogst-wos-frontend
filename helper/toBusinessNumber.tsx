0ab5ec219fb05709e8ea24b741dd64080d3e7da3
Noah Reiter <n.reiter@alpin11.com>
2023-01-02 15:43:59 +0100

    + added hover effect

---
+++ helper/toBusinessNumber.tsx
@@ -0,0 +1,13 @@
+const toBusinessNumber = (number: number) => {
+    const numberString = number.toString().split("").reverse()
+    let result = ""
+    for (let i = 0; i < numberString.length; i++) {
+        if (i % 3 === 0 && i !== 0) {
+            result += "."
+        }
+        result += numberString[i]
+    }
+    return result.split("").reverse().join("")
+}
+
+export default toBusinessNumber
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Grid, Heading, HStack, Image, SimpleGrid, Text } from "@chakra-ui/react";
 import Link from "next/link";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
+import toBusinessNumber from "../helper/toBusinessNumber";
 import fetchData from "../operations/fetcher"
 
 const Home = () => {
@@ -30,16 +31,15 @@ const Home = () => {
   }, [])
 
   return (
-    <Box backgroundColor="white">
+    <Box>
       <Box mx="auto" maxW={{ base: "2xl", lg: "7xl" }} py={{ base: "16", sm: "24" }} px={{ base: "4", sm: "6", lg: "8" }}>
-        <Heading as="h2" p="4">Products</Heading>
+        <Heading as="h2" p="2">Products</Heading>
         <SimpleGrid columns={{ base: 1, sm: 2, lg: 3, xl: 4 }}>
-          {products?.data.products.items.map((product, index) => {
-            console.log(product)
+          {products?.data.products.items.map((product) => {
             return (
-              <Box m="2">
-                <Link key={`${product.assets[0].name}${index}`} href={""}>
-                  <Box h="380px" w="100%">
+              <Box m="2" _hover={{ opacity: 0.75 }} transition="1s">
+                <Link key={`${product.assets[0].name}`} href={""}>
+                  <Box height="380px">
                     <Image
                       src={product.assets[0].source}
                       alt={product.assets[0].name}
@@ -47,19 +47,18 @@ const Home = () => {
                       w="full"
                       objectFit="cover"
                       objectPosition="center"
-                      _groupHover={{ opacity: 0.75 }}
                     />
                   </Box>
                   <HStack justify="space-between" align="center" py="2" fontSize="sm">
                     <Heading as="h3" fontSize="sm" textColor="gray.700">{product.variants[0].name}</Heading>
-                    <Text textColor="gray.900">{product.variants[0].priceWithTax} €</Text>
+                    <Text textColor="gray.900">{toBusinessNumber(product.variants[0].priceWithTax)} €</Text>
                   </HStack>
                 </Link>
               </Box>
             )
           })}
         </SimpleGrid>
-      </Box>
+      </Box >
     </Box >
   )
 }
