319af287636537e031a9e30bb5f18a835a6be7ed
Noah Reiter <n.reiter@alpin11.com>
2023-01-17 07:44:14 +0100

    + added footer

--- compounds/Footer.tsx
+++ compounds/Footer.tsx
@@ -1,11 +1,12 @@
 import { Container, Text, VStack, Box } from "@chakra-ui/react"
+import dayjs from "dayjs"
 
 const Footer = () => {
   return (
     <Box pos="fixed" bottom="0" w="full">
       <Container maxW="container.xl" mb="5">
         <VStack justify="center">
-          <Text color="gray" fontFamily="SFProRegular">© by Noah Reiter | ALPIN11 New Media GmbH 2023</Text>
+          <Text color="gray" fontFamily="SFProRegular">© ALPIN11 New Media GmbH, {dayjs().year()}. Alle Rechte vorbehalten.</Text>
         </VStack>
       </Container>
     </Box>
